e9460919cb5161f5ea486cfbb93bbac512b7f2c1
ksw4193 <165050078+ksw4193@users.noreply.github.com>
2024-07-09 21:39:32 +0900

    drone move
    11

---
+++ Toyproject/Assets/Scripts/DroneNew.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneNew : MonoBehaviour
+{/*
+    public GameObject prefab;
+    public Transform spawnPoint;
+    public int maxInstances = 5;
+    public float spawnInterval = 1f;
+
+    private int instanceCount = 0; // 현재 생성된 객체의 수를 추적하기 위한 변수
+
+    private void Start()
+    {
+        StartCoroutine(SpawnDrones());
+        StopCoroutine(StopDrones());
+    }
+
+    private IEnumerator SpawnDrones()
+    {
+        while (instanceCount < maxInstances)
+        {
+            GameObject instance = Instantiate(prefab, spawnPoint.position, Quaternion.identity);
+            instance.transform.SetParent(spawnPoint);
+            instanceCount++;
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+
+    private IEnumerator StopDrones()
+    {
+        while(5<maxInstances)
+        {
+            yield return 0;
+        }
+    }
+    */
+}
