587fd9a4fb4d997df530813909d90dbce49347c7
whj0623 <whj062379@gmail.com>
2024-07-09 19:35:30 +0900

    asdf

--- Toyproject/Assets/Scripts/Drone.cs
+++ Toyproject/Assets/Scripts/Drone.cs
@@ -5,6 +5,34 @@ using UnityEngine;
 public class Drone : MonoBehaviour
 {
     public float HP = 100;
+    public GameObject bulletPrefab;
+    public Transform firePoint; 
+    private GameObject target;
+    
+
+    private void Start()
+    {
+        target = GameObject.FindWithTag("Player");
+        StartCoroutine(FireCoroutine());
+    }
+    private void Update()
+    {
+        transform.LookAt(target.transform);
+    }
+
+
+    IEnumerator FireCoroutine()
+    {
+        while (true)
+        {
+            Vector3 direction = (target.transform.position - firePoint.position).normalized;
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, Quaternion.LookRotation(direction));
+            Rigidbody rb = bullet.GetComponent<Rigidbody>();
+            if (rb != null)
+                rb.AddForce(direction * 70, ForceMode.VelocityChange);
+            yield return new WaitForSeconds(3);
+        }
+    }
 
     public void GetDamage(float damage)
     {
